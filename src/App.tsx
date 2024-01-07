a7e16894c93bcace45f641aae02c0ed40ab0190a
Marras Antoine <antoine.marras@student-cs.fr>
2024-01-08 00:01:48 +0100

    Added cookie and atomicAge font

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "./fonts/AtomicAge/AtomicAge-Regular.ttf"
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is a test to create a pwa, hope it works
-        </a>
+        <p style={{ "fontFamily": "AtomicAge-Regular" }}>
+          Atomic age font
+        </p>
+        <p style={{ "fontFamily": "Cookie-Regular" }}>
+          Cookie font
+        </p>
       </header>
     </div>
   );
