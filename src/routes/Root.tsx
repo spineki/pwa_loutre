c4ea8e9f318e051c7bc42d6e9c49b32a63d32e2a
spineki <marras.antoine@gmail.com>
2024-03-17 15:20:56 +0100

    Back button takes user back to home screen now

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 import Box from "@mui/material/Box";
 
@@ -9,8 +9,17 @@ import { CloudDialogContextProvider } from "../contexts/CloudDialogContext";
 import { ConversionDialogContextProvider } from "../contexts/ConversionDialogContext";
 import { CloudDialog } from "../dialogs/CloudDialog";
 import { ConversionDialog } from "../dialogs/ConversionDialog";
+import { useEffect } from "react";
 
 export function Root() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    // we want users to go back to the main page wen they use the back button
+    window.onpopstate = () => {
+      navigate("/");
+    };
+  }, []);
+
   return (
     <CloudDialogContextProvider>
       <ConversionDialogContextProvider>
